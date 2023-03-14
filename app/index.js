d560e0153d9469471af77a2a5b7bc7e98aa1a712
samnzay <samnzay@gmail.com>
2023-03-14 11:28:20 +0200

    app update

--- app/index.js
+++ app/index.js
@@ -2,13 +2,13 @@ const app = require("express")();
 const appid = process.env.APPID;
 
 app.get("/", (req,res) => 
-res.send(`appid: ${appid} home page: says hello!`))
+res.send(`appid: ${appid} home page: says hello! Loadbalanced by NGINX`))
 
 app.get("/app1", (req,res) => 
-res.send(`appid: ${appid} app1 page: says hello!`))
+res.send(`appid: ${appid} app1 page: says hello! Loadbalanced by NGINX`))
  
 app.get("/app2", (req,res) => 
-res.send(`appid: ${appid} app2 page: says hello!`))
+res.send(`appid: ${appid} app2 page: says hello! Loadbalanced by NGINX`))
  
 app.get("/admin", (req,res) => 
 res.send(`appid: ${appid} ADMIN page: very few people should see this`))
